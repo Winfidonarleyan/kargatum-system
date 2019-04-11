044e48f92daa9b15408a8e08c303b656caf4ec52
Winfidonarleyan <dowlandtop@yandex.ru>
2019-04-12 00:44:25 +0700

    Anti AD: Fix logic for mute

--- src/mod-anti-advertisment/KargatumAntiAdvertisment.cpp
+++ src/mod-anti-advertisment/KargatumAntiAdvertisment.cpp
@@ -23,6 +23,32 @@ class Kargatum_AntiAdvertisment : public PlayerScript
 public:
     Kargatum_AntiAdvertisment() : PlayerScript("Kargatum_AntiAdvertisment") { }
 
+    void Mute(Player* player, std::string ADMessage, std::string FullMessage)
+    {
+        if (!CONF_BOOL(conf::ANTIAD_MUTE_ENABLE))
+            return;
+
+        if (!CONF_BOOL(conf::ANTIAD_MUTE_GM_ENABLE) && !AccountMgr::IsPlayerAccount(player->GetSession()->GetSecurity()))
+            return;
+
+        ChatHandler handler(player->GetSession());
+        std::string NameLink = handler.GetNameLink(player);
+
+        player->GetSession()->m_muteTime = time(nullptr) + CONF_INT(conf::ANTIAD_MUTETIME) * MINUTE;
+
+        if (CONF_BOOL(conf::ANTIAD_MSG_SELF_ENABLE))
+            handler.PSendSysMessage(lang::ANTIAD_SEND_SELF, CONF_INT(conf::ANTIAD_MUTETIME));
+    }
+
+    void SendGMTexts(std::string NameLink, std::string ADMessage, std::string FullMessage)
+    {
+        if (CONF_BOOL(conf::ANTIAD_MSG_GM_WORLD_ENABLE))
+            sWorld->SendGMText(lang::ANTIAD_SEND_GM_TEXT, NameLink.c_str(), ADMessage.c_str());
+
+        if (CONF_BOOL(conf::ANTIAD_FULLMSG_GM_WORLD_ENABLE))
+            sWorld->SendGMText(lang::ANTIAD_SEND_GM_TEXT_FULL, NameLink.c_str(), FullMessage.c_str());
+    }
+
     void CheckMessage(Player* player, std::string& msg)
     {
         if (!CONF_BOOL(conf::ANTIAD_ENABLE))
@@ -30,35 +56,20 @@ public:
 
         KargatumLoad::ChatMuteContainer& store = sKargatumLoad->GetAntiADMesages();
         std::string CheckMsg = msg;
-        std::string FullMsg = msg;
+        std::string FullMessage = msg;
         CheckMsg.erase(remove_if(CheckMsg.begin(), CheckMsg.end(), ::isspace), CheckMsg.end());
         std::transform(CheckMsg.begin(), CheckMsg.end(), CheckMsg.begin(), ::tolower);
 
         ChatHandler handler(player->GetSession());
         std::string NameLink = handler.GetNameLink(player);
-        uint32 ConfMuteTime = CONF_INT(conf::ANTIAD_MUTETIME);
 
-        for (auto MessageAd : store)
+        for (auto ADMessage : store)
         {
-            if (CheckMsg.find(MessageAd) != std::string::npos)
+            if (CheckMsg.find(ADMessage) != std::string::npos)
             {
-                msg = "";                
-                int64 muteTime = time(nullptr) + ConfMuteTime * MINUTE;
-
-                if (CONF_BOOL(conf::ANTIAD_MSG_GM_WORLD_ENABLE))
-                    sWorld->SendGMText(lang::ANTIAD_SEND_GM_TEXT, NameLink.c_str(), MessageAd.c_str());
-
-                if (CONF_BOOL(conf::ANTIAD_FULLMSG_GM_WORLD_ENABLE))
-                    sWorld->SendGMText(lang::ANTIAD_SEND_GM_TEXT_FULL, NameLink.c_str(), FullMsg.c_str());
-
-                if (CONF_BOOL(conf::ANTIAD_MUTE_ENABLE))
-                {
-                    if (CONF_BOOL(conf::ANTIAD_MUTE_GM_ENABLE) && !AccountMgr::IsPlayerAccount(player->GetSession()->GetSecurity()))
-                        player->GetSession()->m_muteTime = muteTime;
-
-                    if (CONF_BOOL(conf::ANTIAD_MSG_SELF_ENABLE))
-                        handler.PSendSysMessage(lang::ANTIAD_SEND_SELF, ConfMuteTime);
-                }
+                msg = "";
+                SendGMTexts(NameLink, ADMessage, FullMessage);
+                Mute(player, ADMessage, FullMessage);
             }
         }
     };
